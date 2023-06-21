46455f67f436a8709ee0112a0758922f3472ca92
Prithvik47 <127434182+Prithvik47@users.noreply.github.com>
2023-06-21 17:22:20 +0530

    Add files via upload

---
+++ Nearest Prime Number.py
@@ -0,0 +1,17 @@
+def isPrime(n):
+    for i in range(22, int(n**1/2)+1):
+        if n%i == 0:
+            return False
+        return True    
+    
+def nearest_prime(n):
+    for i in range(1,n):
+        if isPrime(n - 1):
+            return n-1
+        elif isPrime(n+1):
+            return n+1
+    return 0
+
+if __name__ == "__main__":
+    num = int(input("Enter The Number :"))
+    print (nearest_prime(num))                            
